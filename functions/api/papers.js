245ac3ca684329f7d7e8dea866a9ec38b7530129
Megan Judge <143248933+megzjudge@users.noreply.github.com>
2026-06-16 23:30:12 +1000

    Update papers.js

--- functions/api/papers.js
+++ functions/api/papers.js
@@ -15,6 +15,18 @@ const STATUSES = ["inbox", "starred", "trash"];
 
 const UMBRELLA_TAGS = {
   "Big Ten": ["Industriousness"],
+  "Indian Psychology": [
+    "Guna", "guna", "Indian", "indian", "Vedic", "vedic",
+    "Sattva", "sattva", "Pancha", "pancha", "Triguna", "triguna", "Atman", "atman",
+  ],
+  "Experimental Philosophy": [
+    "Philosophy of Mind", "philosophy of mind",
+    "Metaphysics", "metaphysics",
+    "Epistemology", "epistemology",
+    "Philosophy of Language", "philosophy of language",
+    "Philosophy of Religion", "philosophy of religion",
+    "experimental philosophy",
+  ],
 };
 
 function umbrellaMembers(tag) {
